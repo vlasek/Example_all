d5f22dc11d9934a8125a2f0bbc1b9fda87351693
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-10-25 19:42:08 +0300

    2

--- Example/Example_001_HelloConsole/Program.cs
+++ Example/Example_001_HelloConsole/Program.cs
@@ -1,2 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
-
+﻿Console.WriteLIne ("введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine ("Привет, ");
+Console.WriteLIne (username);
