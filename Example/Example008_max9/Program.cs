d5c37297a782bef7efb509907e2ff9e3aa7f7d7b
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-10-27 16:44:39 +0300

    1 вариант

---
+++ Example/Example008_max9/Program.cs
@@ -0,0 +1,21 @@
+﻿int a1=15;
+int b1=21;
+int c1=39;
+int a2=12;
+int b2=313;
+int c2=143;
+int a3=452;
+int b3=64;
+int c3=84;
+int max=a1;
+if (b1 > max) max=b1;
+if (c1 > max) max=c1;
+if (a2 > max) max=a2;
+if (b2 > max) max=b2;
+if (c2 > max) max=c2;
+if (a3 > max) max=a3;
+if (b3 > max) max=b3;
+if (c3 > max) max=c3;
+
+Console.Write ("max = ");
+Console.Write (max);
--- Example/Example_007_triangle/Program.cs
+++ Example/Example_007_triangle/Program.cs
@@ -13,25 +13,21 @@ Console.WriteLine ("+");
 int x=xa, y=ya;
 int count =0;
 while (count < 100000)
-{
-int what = new Random().Next(0,3); // 1 2 3
+{ int what = new Random().Next(0,3); // 1 2 3
 if (what==0)
 {
     x=(x+xa)/2;
     y=(y+ya)/2;
-
 }
 if (what==1)
 {
     x=(x+xb)/2;
     y=(y+yb)/2;
-    
 }
 if (what==2)
 {
     x=(x+xc)/2;
     y=(y+yc)/2;
-    
 }
 Console.SetCursorPosition(x,y);
 Console.WriteLine("+");
