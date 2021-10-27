41e969038e89e1d71877d4a31e9c8322bd8f6a5f
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-10-27 12:48:22 +0300

    reshena pro sobaku

---
+++ Example/DZ_dog_2friends/Program.cs
@@ -0,0 +1,29 @@
+﻿int count = 0;
+double distance = 10000;
+double firstFrSpeed = 1;
+double secondFrSpeed = 2;
+double dogSpeed = 5;
+int friend = 2;
+double time = 0;
+
+while (distance > 10)
+{
+
+    if (friend == 1)
+    {
+        time = distance / (firstFrSpeed + dogSpeed);
+        friend = 2;
+        distance = distance - (firstFrSpeed + secondFrSpeed) * time;
+    }
+    else
+    {
+        time = distance / (secondFrSpeed + dogSpeed);
+        friend = 1;
+        distance = distance - (firstFrSpeed + secondFrSpeed) * time;
+    }
+
+
+
+count++;
+}
+Console.WriteLine("Собака пробежит столько раз  "+count);
