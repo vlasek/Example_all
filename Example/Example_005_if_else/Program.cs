310230b90f19f62914a0e734546eb47c497b5989
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-10-27 12:05:27 +0300

    сохранены все

---
+++ Example/Example_005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите имя пользоватля:");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine ("Ура, это же МАША!");
+
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
