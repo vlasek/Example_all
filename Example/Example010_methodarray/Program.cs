07dddccc392436e30cfa706ce10fdf89f219ea95
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-10-27 17:19:12 +0300

    лекция 10

---
+++ Example/Example010_methodarray/Program.cs
@@ -0,0 +1,18 @@
+﻿//int [] array ={14,25,333,354,45, 18, 18, 76,27,48,93};
+
+int n = array.Length;
+int find = 18;
+int index =0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // если нудно один раз
+    }
+    index++; //index =index +1;
+
+}
+
+
