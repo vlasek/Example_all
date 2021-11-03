786e18c88e54915ec290aa4a0f85a4e3ba3b3219
Vladimir Kuzmin <vladimirkuzmin@MacBook-Air.local>
2021-11-03 10:49:35 +0300

    12 b 13 add

---
+++ Example/Example012_Method/Program.cs
@@ -0,0 +1,57 @@
+﻿// // 1 вид
+// void 
+
+// void Method2(string msg)
+// {
+//     Console.WriteLine (msg);
+
+// }
+// Method2 (msg: "Текст сообщения");
+// void Method21(string msg, int count);
+// {
+//     int i=0;
+//     while (i<count)
+//     {
+//         Method2 (msg: "Текст сообщения");
+//         i++;
+//     }
+// }
+// Method2 ("Текст сообщения",4);
+// Method2 (count"Текст сообщения",4); //именнованные аргументы можно писать вне порядка
+
+// int Method3();
+// {
+//     return DateTime.Now.Year;
+// }int year = Method3();
+// Console.WriteLine(year);
+
+//
+// string Method4(int count; string c);
+
+// {
+//     int i=0;
+//     string result = String.Empty;
+//     while 
+//     {result = result+c;
+//         i++;
+//     }
+//     return RegisteredWaitHandle;
+// }
+
+// string res = Method4(10, "asdf");
+// Console.WriteLine(res);
+
+string Method4(int count; string c);
+
+{
+    int i=0;
+    string result = String.Empty;
+    while (i<conut)
+    {result = result+c;
+        i++;
+    }
+    return RegisteredWaitHandle;
+}
+
+string res = Method4(10, "asdf");
+Console.WriteLine(res);
